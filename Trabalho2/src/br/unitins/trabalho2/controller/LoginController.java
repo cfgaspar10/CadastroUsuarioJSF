ae9cde70fe7a66cc5faf67082ece0c897c15812f
carlesandrogaspar <carlesandrogaspar@172.16.109.221>
2019-08-21 08:44:26 -0300

    inclusao funções CRUD

--- Trabalho2/src/br/unitins/trabalho2/controller/LoginController.java
+++ Trabalho2/src/br/unitins/trabalho2/controller/LoginController.java
@@ -16,35 +16,34 @@ import br.unitins.trabalho2.model.Usuario;
 public class LoginController {
 
 	private Usuario usuario;
-	
+
 	private List<Usuario> listaUsuario;
-	
-	public List<Usuario> getListaUsuario(){
+
+	public List<Usuario> getListaUsuario() {
 		if (listaUsuario == null) {
 			listaUsuario = new ArrayList<Usuario>();
-			listaUsuario.add(new Usuario("Teste","teste","123"));
-			listaUsuario.add(new Usuario("User","User","123"));
+			listaUsuario.add(new Usuario("Teste", "teste", "123"));
+			listaUsuario.add(new Usuario("User", "User", "123"));
 		}
 		return listaUsuario;
 	}
 
 	public void entrar() {
-		for(Usuario u: getListaUsuario()) {
+		for (Usuario u : getListaUsuario()) {
 			if (u.getNome().equals(getUsuario().getNome()) && u.getLogin().equals(getUsuario().getLogin())
-						&& u.getSenha().equals(getUsuario().getSenha())) {
-					Util.redirect("menu.xhtml");
-				}
+					&& u.getSenha().equals(getUsuario().getSenha())) {
+				Util.redirect("menu.xhtml");
+			}
 			System.out.println(getUsuario().getNome());
 			System.out.println(getUsuario().getLogin());
 			System.out.println(getUsuario().getSenha());
-			}
+		}
 		Util.addMessageError("Usuario ou Senha Inválido");
 //		return null;
-		}
+	}
 
-	
 	public Usuario getUsuario() {
-		if(usuario == null) {
+		if (usuario == null) {
 			usuario = new Usuario();
 		}
 		return usuario;
@@ -53,23 +52,30 @@ public class LoginController {
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	
+
 	public void incluir() {
 		getListaUsuario().add(getUsuario());
 		System.out.println(getUsuario().getNome());
 		System.out.println(getUsuario().getLogin());
 		System.out.println(getUsuario().getSenha());
 		System.out.println(getUsuario().getAtivo());
-		
+
 		limpar();
-		//Util.addMessageError("Campos vazios, por favor preencha os dados.");
+		// Util.addMessageError("Campos vazios, por favor preencha os dados.");
 	}
-	
+
+	public void alterar() {
+
+	}
+
+	public void excluir() {
+
+	}
+
 	public void limpar() {
 		setUsuario(null);
 	}
-	
-	
+
 //	public String logar() {
 //		if (getUsuario().getLogin().equals("teste")
 //				&& getUsuario().getSenha().equals("123")) {
@@ -81,9 +87,5 @@ public class LoginController {
 //		Util.addMessageError("Usuario ou Senha Inválido");
 //		return null;
 //	}
-	
-	
-
-	
 
 }
